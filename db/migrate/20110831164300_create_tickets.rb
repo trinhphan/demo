51c1dc95bd7a26a60f2ab6d665e9efd12f0675fb
trinh phan <trinhphan@jv-it.com.vn>
2011-09-01 00:20:10 +0700

    create ticket

---
+++ db/migrate/20110831164300_create_tickets.rb
@@ -0,0 +1,14 @@
+class CreateTickets < ActiveRecord::Migration
+  def self.up
+    create_table :tickets do |t|
+      t.string :name
+      t.string :price
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :tickets
+  end
+end
